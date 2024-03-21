f8a44051fd9eb165f2ae52fba20047661b432a6c
Bo Wang <bow2@ece017.ece.local.cmu.edu>
2024-03-21 00:27:26 -0400

    commit the template for openmp and cuda

---
+++ openmp/CPP/Utils/CommonUtils.h
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include "clipper2/clipper.h"
+#ifndef __COMMONUTILS_H__
+#define __COMMONUTILS_H__
+
+Clipper2Lib::Path64 MakeRandomPoly(int width, int height, unsigned vertCnt)
+{
+  using namespace Clipper2Lib;
+  Path64 result;
+  result.reserve(vertCnt);
+  for (unsigned i = 0; i < vertCnt; ++i)
+    result.push_back(Point64(std::rand() % width, std::rand() % height));
+  return result;
+}
+
+Clipper2Lib::PathD MakeRandomPolyD(int width, int height, unsigned vertCnt)
+{
+  using namespace Clipper2Lib;
+  PathD result;
+  result.reserve(vertCnt);
+  for (unsigned i = 0; i < vertCnt; ++i)
+    result.push_back(PointD(std::rand() % width, std::rand() % height));
+  return result;
+}
+
+#endif
