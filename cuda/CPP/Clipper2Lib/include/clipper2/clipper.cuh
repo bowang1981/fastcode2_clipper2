5f1bd9196640686d8fc32ebc750fd4795f15a3d0
Bo Wang <bow2@ece017.ece.local.cmu.edu>
2024-04-18 18:35:11 -0400

    add basic structure for path on cuda

--- cuda/CPP/Clipper2Lib/include/clipper2/clipper.cuh
+++ cuda/CPP/Clipper2Lib/include/clipper2/clipper.cuh
@@ -1,7 +1,16 @@
 #ifndef CLIPPER_CUH_
 #define CLIPPER_CUH_
 #include <stdio.h>
+#include <vector>
+#include "clipper.core.h"
+// #include "clipper.h"
+//#include "clipper.rectclip.h"
+// #include "clipper.offset.h"
+
+namespace Clipper2Lib {
 
 void wrap_test_print();
+float area_single(const Path64& path);
 
+}
 #endif
